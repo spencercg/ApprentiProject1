8d74da989fdf05b61800e52f4477848975ef1d48
spencercg <s.grimes.nv@gmail.com>
2024-11-11 15:41:22 -0800

    Updating project

--- Book.py
+++ Book.py
@@ -4,6 +4,31 @@ class Book:
     isbn: str
     available: bool
 
+
+# I need to update borrow and return so that status of availability actually changes
+
+
+    def borrow_book(self):
+        borrow_request = input("Please enter the title of the book you would like to request: ")
+        Book.available = False
+        print(borrow_request + " has been successfully checked out.")
+        print(Book.available)
+
+    def return_book(self):
+        return_request = input("Please enter the title of the book you are returning: ")
+        Book.available = True
+        print(return_request + " has been successfully returned")
+
+
+
+
+    def create_book(self):
+        self.title = input("Please enter the title of the new book: ")
+        print(self.title)
+
+
+
+
     def __init__(self, title, author, isbn, available):
         self.title = title
         self.author = author
@@ -11,6 +36,11 @@ class Book:
         self.available = available
 
 
-Moby_Dick = Book("Moby Dick", "Herman Melville", "12341234", True)
 
 
+
+# Moby_Dick = Book("Moby Dick", "Herman Melville", "12341234", True)
+
+# Moby_Dick.borrow_book()
+
+# print(Moby_Dick.available)
---
+++ Library.py
@@ -0,0 +1,7 @@
+class Library:
+    books_list = list
+    users_list = list
+
+
+#    def add_book(self):
+#        new_book = input("Please enter the follow")
