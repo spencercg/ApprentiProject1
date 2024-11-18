ab8a7255ad0387ae336b810dd0176dced35d4644
spencercg <s.grimes.nv@gmail.com>
2024-11-18 15:17:27 -0800

    Updating unit test

--- test_Library.py
+++ test_Library.py
@@ -4,16 +4,19 @@ from Book import Book
 from Library import Library
 
 
-'''
+
 test_library_book_list = []
 
 class TestUser(unittest.TestCase):
     
     def test_library_add_book(self):
+
         new_test_book = Library.add_book()
         
         test_library_book_list.append(new_test_book)
 
         self.assertIn(new_test_book, test_library_book_list)
         
-        '''
+
+if __name__ == '__main__':
+    unittest.main()
